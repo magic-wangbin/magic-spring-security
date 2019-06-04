a2b8e9596aa37e2cd6d76f47aaa613905cd191c7
wang.bin <wang.bin@ielpm.com>
2019-06-04 14:48:03 +0800

    'AppSecurityController'

--- magic-spring-security-app/src/main/java/com/magic/security/app/controller/AppSecurityController.java
+++ magic-spring-security-app/src/main/java/com/magic/security/app/controller/AppSecurityController.java
@@ -1,28 +1,47 @@
 package com.magic.security.app.controller;
 
 import com.magic.security.app.social.AppSingUpUtils;
+import com.magic.security.core.properties.SecurityProperties;
 import com.magic.security.core.properties.constans.SecurityConstants;
 import com.magic.security.core.social.controller.SocialController;
+import com.magic.security.core.support.SimpleResponse;
 import com.magic.security.core.support.SocialUserInfo;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.web.ProviderSignInUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.ServletWebRequest;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 public class AppSecurityController extends SocialController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private AppSingUpUtils appSingUpUtils;
 
     @Autowired
     private ProviderSignInUtils providerSignInUtils;
+
+    private RequestCache requestCache = new HttpSessionRequestCache();
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @Autowired
+    private SecurityProperties securityProperties;
 
     /**
      * 需要注册时跳到这里，返回401和用户信息给前端
@@ -36,5 +55,27 @@ public class AppSecurityController extends SocialController {
         Connection<?> connection = providerSignInUtils.getConnectionFromSession(new ServletWebRequest(request));
         appSingUpUtils.saveConnectionData(new ServletWebRequest(request), connection.createData());
         return buildSocialUserInfo(connection);
+    }
+
+    /**
+     * 需要登录的请求跳转或提示.
+     *
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping("/authentication/require")
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public SimpleResponse requireAuthentication(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        SavedRequest savedRequest = requestCache.getRequest(request, response);
+        if (savedRequest != null) {
+            String redirectUrl = savedRequest.getRedirectUrl();
+            logger.info("引发跳转的url地址为：" + redirectUrl);
+            if (StringUtils.endsWithIgnoreCase(redirectUrl, ".html")) {
+                redirectStrategy.sendRedirect(request, response,
+                    securityProperties.getBrowser().getLoginPage());
+            }
+        }
+        return new SimpleResponse("访问的服务需要身份认证，请先进行登录!");
     }
 }
